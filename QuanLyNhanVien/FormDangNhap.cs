49900f5249bf0eed2236dd670df92511ec9608d2
hieubeo97 <35682846+hieubeo97@users.noreply.github.com>
2018-03-12 14:20:28 +0700

    Form Dang Nhap

--- QuanLyNhanVien/FormDangNhap.cs
+++ QuanLyNhanVien/FormDangNhap.cs
@@ -17,21 +17,65 @@ namespace QuanLyNhanVien
         {
             InitializeComponent();
         }
-        
+
+
+        SqlConnection con;
+        public static string TK = null;
+        public static string MK = null;
+        public static int id;
+
 
         private void FormDangNhap_Load(object sender, EventArgs e)
         {
-         
+            string conString = @"Data Source=DESKTOP-8J6L2ID\SQLEXPRESS;Initial Catalog=QuanLyNhanVien;Integrated Security=True";
+            con = new SqlConnection(conString);
         }
 
         private void btnDangNhap_Click(object sender, EventArgs e)
         {
-           
+            TK = txtTK.Text;
+            MK = txtTK.Text;
+            try
+            {
+                string sql = "Select Count(*) From [QuanLyNhanVien].[dbo].[TaiKhoan] Where TenTK=@acc And MatKhau=@pass ";
+
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.Add(new SqlParameter("@acc", TK));
+                cmd.Parameters.Add(new SqlParameter("@pass", MK));
+                int x = (int)cmd.ExecuteScalar();
+                while (true)
+                {
+                    if (x == 1)
+                    {//đăng nhập thành công
+
+                        FormChinh a = new FormChinh();
+                        a.ShowDialog();
+                        con.Close();
+                        break;
+
+                    }
+                    else
+                    {
+                        //đăng nhập thất bại
+                        labelThongBao.Text = "Thông tin nhập sai vui lòng nhập lại";
+                        txtTK.Text = "";
+                        txtMK.Text = "";
+                        txtTK.Focus();
+                        con.Close();
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnThoat_Click(object sender, EventArgs e)
         {
-           
+            Application.Exit();
         }
     }
 }
